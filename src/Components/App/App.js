68921514462d57bf4246d795afbe5aff28b6f268
luko <luko.web@gmail.com>
2019-10-24 00:11:38 -0300

    feat: Add App.css file

---
+++ src/Components/App/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Spotify Searcher</h1>
+      <div class="App">
+        <div class="App-playlist">
+
+          
+          {/* 🚩  NOTE: import this components */}
+          {/* Add a Playlist component  */}
+          {/* Add a SearchBar component  */}
+          {/*Add a SearchResults component  */}
+
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
