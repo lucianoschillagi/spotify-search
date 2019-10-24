8ea371a2c7c4298d1bcddf8fd67a380c3f9ca073
luko <luko.web@gmail.com>
2019-10-24 10:41:43 -0300

    refactor: In App component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Spotify Searcher</h1>
-      <div className="App">
-        <div className="App-playlist">
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+  }
 
-          
-          {/* 🚩  NOTE: import this components */}
-          {/* Add a Playlist component  */}
-          {/* Add a SearchBar component  */}
-          {/*Add a SearchResults component  */}
-
+  render() {
+      return (
+        <div>
+          <h1>Spotify Searcher</h1>
+          <div className="App">
+            <div className="App-playlist">
+    
+              
+              {/* 🚩  NOTE: import this components */}
+              {/* Add a Playlist component  */}
+              {/* Add a SearchBar component  */}
+              {/*Add a SearchResults component  */}
+    
+              </div>
           </div>
-      </div>
-    </div>
-  );
+        </div>
+      );
+  }
 }
 
 export default App;
