28b753816b416c5948c908a4628fb1339ca0fc69
luko <luko.web@gmail.com>
2019-10-24 10:13:13 -0300

    feat: Add an initial TrackList component

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// TODO: import Track component
+
+// an stateLESS class component
+// Represents a list of tracks (results and user's playlist)
+class TrackList extends Component {
+  render() {
+    return (
+      <div className="TrackList">
+
+      </div>
+    );
+  }
+}
+
+export default TrackList;
