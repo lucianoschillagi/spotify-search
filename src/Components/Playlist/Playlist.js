c7cd0cebe9cd0a43691cae6117a3b281532fe61d
luko <luko.web@gmail.com>
2019-10-24 10:03:22 -0300

    feat: Add a initial Playlist component

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+// import TrackList ...
+
+// Respresents the playlist created for the user.
+class Playlist extends Component { // a stateLESS class component
+  render() {
+    return (
+      <div className="Playlist">
+
+        {/* 1-The name of the playlist */}
+        <input value={'New Playlist'}/>
+
+        {/* 2-Add a TrackList component */}
+
+        {/* 3-Save to Spotify Button */}
+        <button class="Playlist-save">
+          SAVE TO SPOTIFY
+        </button>
+
+      </div>
+    );
+  }
+}
+
+export default Playlist;
